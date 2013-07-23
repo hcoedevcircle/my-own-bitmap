c0153fd7a9877d91d1ec49e8f74d34e1e5e011f2
Nootan Ghimire <nootan.ghimire@gmail.com>
2013-07-23 10:03:27 -0700

    Initial commit

---
+++ writebm.c
@@ -0,0 +1,41 @@
+/*writebm.c -- writes bitmap in files (not standard bitmap) */
+
+#include<stdio.h>
+#include<conio.h>
+
+typedef struct {
+  int char_id;            // Char_id
+  int char_bmap[16];  // The real character bitmap
+  } xbmp;
+
+void main() {
+  FILE *fp;
+  int maxlen, i, j;   //MaxLength and legacy counters :)
+  xbmp myBitmap;
+
+  printf("\n\t\t\tBitmap Writer\n\t\t\t=============\n\n");
+
+  printf(" Enter No. Of Characters you want to write ");
+
+  scanf("%d",&maxlen);
+
+  fp = fopen("test.xbmp","ab");
+
+    for(i = 0; i<maxlen; i++) {
+        printf("\n |||Entering For Character %d|||\n\n", i+1);
+        myBitmap.char_id = i; //Assigning Our Own Char id :D
+        printf("Enter Character Bitmap Now::::");
+        for(j=0; j<=15; j++) { //This is understandable, aye?
+        printf("\nEnter Hex (in 0x00 format) For Row %d: ", j+1);
+        scanf("%x",&myBitmap.char_bmap[j]);
+        } //End j loop
+
+   fwrite(&myBitmap, sizeof(myBitmap), 1, fp);
+
+   printf("\n----------File Append Complete-------\n");
+
+   } //End i loop
+
+  fclose(fp); //close a file
+  getch();
+}
